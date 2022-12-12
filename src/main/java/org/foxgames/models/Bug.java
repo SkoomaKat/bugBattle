e1e1e0e870c91a7cf17b3ca928b309b6a06a107a
Andrew Drennan <jadrennan2@gmail.com>
2022-12-12 11:25:30 -0800

    Add Bug variables and methods according to class diagram.

--- src/main/java/org/foxgames/models/Bug.java
+++ src/main/java/org/foxgames/models/Bug.java
@@ -1,4 +1,26 @@
 package org.foxgames.models;
 
 public class Bug {
+  private String name;
+  private String type;
+  private int maxHealth;
+  private int curHealth;
+  private int defence;
+  private int attack;
+
+  public Bug() {
+
+  }
+  public void attack(Bug bug) {
+
+  }
+  public void defend() {
+
+  }
+  public void heal(Integer quantity) {
+
+  }
+  public void heal() {
+
+  }
 }
